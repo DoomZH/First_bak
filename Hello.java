c487afe60689032133553a386d37fb82fb34af26
doom <2504657875@qq.com>
2016-06-14 15:11:23 +0800

    update Hello

--- Hello.java
+++ Hello.java
@@ -1,15 +1,9 @@
-﻿public class Hello{
-	
-	public static void main(String args){
-		
-		System.out.println("Hello1");
-		
-		System.out.println("Hello2");
-		
-		System.out.println("Hello32212");
-		
-		System.out.println("Hello32212kK");
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("123");
+		System.out.println("124");
+		System.out.println("125");
+		System.out.println("126");
 	}
-	
-	
 }
